25417a04e78e071e871a96a1a1abea90f41562e3
ImWillIdk <devesaguillem@gmail.com>
2023-04-27 22:57:22 +0200

    b64 --update

--- js/index.js
+++ js/index.js
@@ -1,10 +1,27 @@
 var text = null;
 var password = null;
+var final_text = null;
+var sel = null;
 
 function encrypt() {
     text = document.getElementById("input").value;
     password = document.getElementById("password").value;
-    document.getElementById("result").innerHTML = text;
+    sel = document.getElementById("method").value;
+    if (sel == "b64") {
+        final_text = btoa(text)
+    }
+    document.getElementById("result").innerHTML = final_text;
+    document.getElementById("titleresult").innerHTML = "Texto Cifrado";
+}
+
+function decrypt() {
+    text = document.getElementById("input").value;
+    password = document.getElementById("password").value;
+    sel = document.getElementById("method").value;
+    if (sel == "b64") {
+        final_text = atob(text)
+    }
+    document.getElementById("result").innerHTML = final_text;
     document.getElementById("titleresult").innerHTML = "Texto Cifrado";
 }
 
